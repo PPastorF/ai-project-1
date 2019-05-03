54793e6716d04222be8769224bb2733eac759233
PPastorF <pedropastor999@gmail.com>
2019-05-03 01:27:44 -0300

    A* fixed and did best-first

--- project1.py
+++ project1.py
@@ -1,36 +1,36 @@
 from src.graph import Graph
 from src.map_parser import read_filemap, create_graph
 import json
-from pprint import PrettyPrinter as PP
 
 def main():
 	arr = read_filemap('maps/map1.txt')
 	grafo = create_graph(arr)
 
 def test():
-	pp = PP()
-
 	arr = read_filemap('maps/map1.txt')
 
-	arr = [
-		['#', '*', '*'],
-		['*', '*', '*'],
-		['*', '*', '$']
-	]
+	# arr = [
+	# 	['#', '*', '*'],
+	# 	['*', '*', '*'],
+	# 	['*', '*', '$']
+	# ]
 
 	grafo = create_graph(arr)
 	if grafo == None:
 		return
 
 	print( 'Caminho entre '+grafo.start_node+' e '+grafo.end_node+':' )
-	print( "DFS:" )
-	print( grafo.depth_first(grafo.start_node, grafo.end_node)[::-1] )
+	# print( "DFS:" )
+	# print( grafo.depth_first(grafo.start_node, grafo.end_node)[::-1] )
 
 	# print( "BFS:" )
 	# print( grafo.breadth_first(grafo.start_node, grafo.end_node) )
 	
 	print( "A STAR:" )
 	print( grafo.a_star(grafo.start_node, grafo.end_node) )
+
+	print( "BEST FIRST:" )
+	print( grafo.best_first(grafo.start_node, grafo.end_node) )
 
 if __name__ == '__main__':
 	test()
--- src/graph.py
+++ src/graph.py
@@ -1,7 +1,6 @@
 from math import sqrt
 from ast import literal_eval
 from src.tools import *
-from pprint import PrettyPrinter as PP
 
 class Graph(object):
 
@@ -78,13 +77,18 @@ class Graph(object):
 
 	def breadth_first(self, start, end):
 		queue = [[start]]
-		
+		visited = []
 		####
 		while queue:
 			path = queue.pop(0)
 
 			####
 			node = path[-1]
+			
+			if node in visited:
+				continue
+
+			print(node)
 			if node == end:
 				return path
 
@@ -98,21 +102,98 @@ class Graph(object):
 
 				queue.append(enqueue)
 
+			visited.append(node)
 
-	def best_first():
-		pass
-
-	def a_star(self, start, end):
-		pp = PP(depth=4)
-
+	def best_first(self, start, end):
 		###
 		node_data  = {}
-		fringe     = []
+		open_nodes = []
 		path       = []
 		done 	   = []
 		found = False
 
-		###				
+		###
+		h = euclidian_distance(start, end)
+		start_node = {
+			start: {
+				"h": h,
+				"predecessor": None
+			}
+		}
+
+		open_nodes.append( (start, h) )
+		node_data.update(start_node)
+
+		###
+		while open_nodes:
+			###
+			open_nodes.sort(key=lambda tup: tup[1])
+			open_nodes = open_nodes[::-1]
+			current_node = open_nodes.pop(0)[0]
+
+			if current_node == end:
+				found = True
+				break
+
+			if current_node in done:
+				continue
+
+			dist = euclidian_distance(current_node, end)
+
+			###
+			node_edges = self.struct[current_node]
+			for edge in node_edges:
+				dest_node   = edge[0]
+				edge_weight = edge[1]
+
+				if dest_node in done:
+					continue
+
+				if dest_node not in open_nodes:
+					open_nodes.append( (dest_node, h) )
+
+				h = dist + edge_weight
+
+				###
+				adjacent = {
+					dest_node: {
+						"h": h,
+						"predecessor": current_node		
+					}
+				}
+
+				###
+				if dest_node not in node_data.keys():
+					node_data.update(adjacent)
+				elif h < node_data[dest_node]['h']:
+					node_data[dest_node] = adjacent[dest_node]
+
+			done.append(current_node)
+
+		###
+		if found:
+			path.append(end)
+			current_node = node_data[end]
+
+			while current_node['predecessor'] != None:
+				pred = current_node['predecessor']
+				path.append(pred)
+				current_node = node_data[pred]
+
+			return path[::-1]
+		
+		else:
+			return None
+
+	def a_star(self, start, end):
+		###
+		node_data  = {}
+		open_nodes = []
+		path       = []
+		done 	   = []
+		found = False
+
+		###
 		g = 0
 		f = g + euclidian_distance(start, end)
 		start_node = {
@@ -123,14 +204,15 @@ class Graph(object):
 			}
 		}
 
-		fringe.append( (start, f) )
+		open_nodes.append( (start, f) )
 		node_data.update(start_node)
 
 		###
-		while fringe:
+		while open_nodes:
 			###
-			fringe.sort(key=lambda tup: tup[1])
-			current_node = fringe.pop(0)[0]
+			open_nodes.sort(key=lambda tup: tup[1])
+			open_nodes = open_nodes[::-1]
+			current_node = open_nodes.pop(0)[0]
 
 			if current_node == end:
 				found = True
@@ -150,8 +232,8 @@ class Graph(object):
 				if dest_node in done:
 					continue
 
-				if dest_node not in fringe:
-					fringe.append( (dest_node, f) )
+				if dest_node not in open_nodes:
+					open_nodes.append( (dest_node, f) )
 
 				g = node_data[current_node]['g'] + edge_weight
 				f = g + h
